76b0acaa492c8859e28ae01dc10e182f46f06c85
ryuc1986 <cho4649diver@yahoo.co.jp>
2019-12-01 14:56:01 +0900

    Update operation.sh

--- operation.sh
+++ operation.sh
@@ -2,19 +2,38 @@
 
 ##################
 #bootloader
- 
+#?
 #os
+if `dmesg | grep 'redhat' or 'centos''`
+ cat /etc/redhat-release
+ 
 #kernel
+uname -r
+
 #shell
+echo $SHELL
+
 #loginname
+logname
+
 #username
+whoami
+
 #runlevel
+who -r
 
 #######################
 #hostname
+uname -n
+
 #ipaddress
+ifconfig -a eth0 | grep -v inet6 | grep inet
+
 #dns　正引き/逆引き
+#nslookup
+
 #connection
+#ping 
 
 #####################
 #filesystem
@@ -38,7 +57,10 @@
 #kernel module list number
 #package list number
 #user list number
-#mo
+#lock list number
+#mount list number
+#kernel module list number
+#kernel module loading list number
 
 ###############
 #error log
