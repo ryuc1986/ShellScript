11b350e86fa95b6a0fe94e7e3ce3c51b89c304b6
ryuc1986 <cho4649diver@yahoo.co.jp>
2019-12-01 14:06:59 +0900

    Create operation.sh

---
+++ operation.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+##################
+#bootloader
+ 
+#os
+#kernel
+#shell
+#loginname
+#username
+#runlevel
+
+#######################
+#hostname
+#ipaddress
+#dns　正引き/逆引き
+#connection
+
+#####################
+#filesystem
+#mount
+
+##############
+#hardware resorce
+#cpu
+#memory
+#disk
+#network
+
+################
+#hardware info
+
+######################
+#active application
+
+###############
+#statification list
+#kernel module list number
+#package list number
+#user list number
+#mo
+
+###############
+#error log
+
+
+################
+#last login history
+
+
+
